63b94e73b142dbd7fa2b288ad5491b9cadc61767
PreshitS <satalgaonkarpreshit@gmail.com>
2023-12-06 02:00:30 +0530

    after day 4

--- 4-dec.py
+++ 4-dec.py
@@ -23,3 +23,29 @@ for i in range(len(lines)):
 print(tot)
 
 #second prob
+with open("input.txt") as f:
+    lines = f.readlines()
+
+abc = [1] * (len(lines) + 10)
+for i in range(len(lines)):
+    lst = lines[i].split(":")
+    arr = lst[1].split("|")
+    win = arr[0].split()
+    for j in range(len(win)):
+        win[j] = int(win[j])
+    allc = arr[1].split()
+    for j in range(len(allc)):
+        allc[j] = int(allc[j])
+    count = 0
+    for j in range(len(allc)):
+        if allc[j] in win:
+            count += 1
+    for k in range(abc[i]):
+        djfsk = count
+        j = i + 1
+        while djfsk:
+            abc[j] += 1
+            j += 1
+            djfsk -= 1
+
+print(sum(abc[:-10]))
